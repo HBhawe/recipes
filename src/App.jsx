74349ca678ed876f6bec11790235e4b01a755d16
Harshal Bhawe <harshalb97@outlook.com>
2025-10-08 17:46:49 +0200

    Added biome and config

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import recipes from './data/recipes.json'
+import { useState } from "react";
+import "./App.css";
+import recipes from "./data/recipes.json";
 
-console.log(recipes)
+console.log(recipes);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Recipes</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>Recipes</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
